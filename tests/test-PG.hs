7d2d20b2ea684bf9ba704172b5ecc8864dbdcbd4
Andrey Mokhov <andrey.mokhov@gmail.com>
2013-01-15 20:34:43 +0000

    Testing PGs.

--- tests/test-PG.hs
+++ tests/test-PG.hs
@@ -5,7 +5,25 @@
 	Description: Testing PGs.
 -}
 
-test :: Bool -> IO ()
-test True = putStrLn "OK"
-test False = putStrLn "FAIL"
+import Test.Framework (defaultMain, testGroup)
+import Test.Framework.Providers.HUnit
+import Test.Framework.Providers.QuickCheck (testProperty)
 
+import Test.QuickCheck
+import Test.HUnit
+
+main = defaultMain tests
+
+tests = [
+		testGroup "Equality tests"
+			[
+			testCase "Epsilon" test_sort7
+			],
+	    testGroup "Sorting Group 2" [
+	                testProperty "sort4" prop_sort4,
+	                testProperty "sort5" prop_sort5,
+	                testProperty "sort6" prop_sort6,
+	                testCase "sort7" test_sort7,
+	                testCase "sort8" test_sort8
+            ]
+    ]
